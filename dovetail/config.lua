d7bc3d920d107312213951003a8574b4cd686dc4
James Reed <jcrd@tuta.io>
2021-02-20 14:07:30 -0700

    dovetail: Fix `cmd.client.unminimize`

--- dovetail/config.lua
+++ dovetail/config.lua
@@ -115,6 +115,7 @@ keys {
     ['M-f'] = cmd.client.focus.other,
     ['M-S-f'] = cmd.client.focus.other_layer,
     ['M-o'] = cmd.client.toggle_max,
+    ['M-S-x'] = cmd.client.unminimize,
 
     -- Session.
     ['M-BackSpace'] = cmd.session.lock,
@@ -153,7 +154,6 @@ client_keys {
     ['M-n'] = cmd.client.normalize,
     ['M-z'] = cmd.client.replace,
     ['M-x'] = cmd.client.minimize,
-    ['M-S-x'] = cmd.client.unminimize,
 }
 
 buttons {
