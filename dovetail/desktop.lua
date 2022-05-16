28b5765bd0832ee037289ca6d6aab506f3ab9e96
James Reed <james@twiddlingbits.net>
2022-05-16 11:24:19 -0600

    Remove extraneous configs

--- dovetail/desktop.lua
+++
@@ -1,4 +0,0 @@
-options {
-    inc_brightness_cmd = 'sudo ddcutil setvcp 10 + %d',
-    dec_brightness_cmd = 'sudo ddcutil setvcp 10 - %d',
-}
--- dovetail/laptop.lua
+++
@@ -1,6 +0,0 @@
-options {
-    brightness_step = 200,
-    battery_low_percent = 10,
-    battery_charged_percent = 95,
-    enable_battery_widget = true,
-}
